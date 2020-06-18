a594f78dc2b1c41e8463c7fa9520f003aa16644b
student46 <frank55669test@outlook.com>
2020-06-18 12:49:24 +0800

    +googleMaps

---
+++ NoteApp/GoogleMapVC.swift
@@ -0,0 +1,30 @@
+//
+//  GoogleMapVC.swift
+//  NoteApp
+//
+//  Created by student46 on 2020/6/18.
+//  Copyright © 2020 Frank. All rights reserved.
+//
+
+import UIKit
+
+class GoogleMapVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
